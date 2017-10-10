3c41ce4bd4c343afbbe111473c3063eb8eb495d3
Brook Blue <blueb@oregonstate.edu>
2017-10-10 16:17:20 -0700

    Attempt at bouncing human additional. Need to resolve issue.

--- sketch.js
+++ sketch.js
@@ -7,11 +7,19 @@ var zombieDamping = -0.5;
 var zombieSize = 80;
 var zombieColor;
 var backgroundColor;
+var humanY = 100;
+var humanV = 0;
+var humanA = 0.2;
+var humanDamping = -0.5;
+var humanSize = 80;
+var humanColor;
+
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	backgroundColor = color(204, 255, 204);
 	zombieColor = color(174, 120, 174);
+	humanColor = color(174, 0, 0);
 }
 
 function draw() {
@@ -25,4 +33,13 @@ function draw() {
 		zombieY = windowHeight - (zombieSize);
 		zombieV *= zombieDamping;
 		zombieSize *= 0.8;
+
+	fill(humanColor);
+	ellipse(windowWidth / 2, humanY, humanSize, humanSize);
+	humanY += humanV;
+	humanV += humanA;
+	if (humanY + (humanSize / 2) >= windowHeight);
+		humanY = windowHeight - (zombieSize);
+		humanV *= humanDamping;
+		humanSize *= 0.8;
 }
