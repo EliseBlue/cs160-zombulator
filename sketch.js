4ef4b664b04eb9c866f4a1b7786f60dbfc87c798
Brook Blue <blueb@oregonstate.edu>
2017-10-11 17:29:34 -0700

    Move and draw shortcut functions added. Still moving slow on computer. Need to resolve.

--- sketch.js
+++ sketch.js
@@ -6,40 +6,62 @@ var zombieA = 0.2;
 var zombieDamping = -0.5;
 var zombieSize = 80;
 var zombieColor;
-var backgroundColor;
+
 var humanY = 100;
 var humanV = 0;
-var humanA = 0.2;
-var humanDamping = -0.5;
+var humanA = 0.6;
+var humanDamping = -0.8;
 var humanSize = 80;
 var humanColor;
 
+var backgroundColor;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	backgroundColor = color(204, 255, 204);
-	zombieColor = color(174, 120, 174);
-	humanColor = color(174, 0, 0);
+  createCanvas(windowWidth, windowHeight);
+  backgroundColor = color(114, 168, 255);
+  zombieColor = color(242, 255, 0);
+  humanColor = color(random(200, 255), random(200, 255), random(200, 255));
 }
 
 function draw() {
-	background(backgroundColor);
-	noStroke();
-	fill(zombieColor);
-	ellipse(windowWidth / 2, zombieY, zombieSize, zombieSize);
-	zombieY += zombieV;
-	zombieV += zombieA;
-	if (zombieY + (zombieSize / 2) >= windowHeight);
-		zombieY = windowHeight - (zombieSize);
-		zombieV *= zombieDamping;
-		zombieSize *= 0.8;
+  background(backgroundColor);
+  noStroke();
 
-	fill(humanColor);
-	ellipse(windowWidth / 2, humanY, humanSize, humanSize);
-	humanY += humanV;
-	humanV += humanA;
-	if (humanY + (humanSize / 2) >= windowHeight);
-		humanY = windowHeight - (zombieSize);
-		humanV *= humanDamping;
-		humanSize *= 0.8;
+  drawZombie();
+  moveZombie();
+  drawHuman();
+  moveHuman();
+
+}
+
+function drawZombie() {
+  fill(zombieColor);
+  ellipse(windowWidth / 2, zombieY, zombieSize, zombieSize);
+}
+ 
+function moveZombie() { 
+  zombieY += zombieV;
+  zombieV += zombieA;
+  if (zombieY + (zombieSize / 2) >= windowHeight) {
+    zombieY = windowHeight - (zombieSize / 2);
+    zombieV *= zombieDamping;
+}
+}
+
+
+
+function drawHuman() {
+  fill(humanColor);
+  ellipse(windowWidth / 4, humanY, humanSize, humanSize);
+  fill(0);
+  text("human", windowWidth / 4, humanY);
+}
+
+function moveHuman() {
+  humanY += humanV;
+  humanV += humanA;
+  if (humanY + (humanSize / 2) >= windowHeight) {
+    humanY = windowHeight - (humanSize / 2);
+    humanV *= humanDamping;
+}
 }
